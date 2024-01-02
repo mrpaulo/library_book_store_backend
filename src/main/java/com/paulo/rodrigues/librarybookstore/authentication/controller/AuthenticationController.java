72181834946401d860204f6b5bd287c36a4c3d71
Paulo Rodrigues <mr.paulo@aluno.faccat.br>
2024-01-02 20:30:15 -0300

    Removed import

--- src/main/java/com/paulo/rodrigues/librarybookstore/authentication/controller/AuthenticationController.java
+++ src/main/java/com/paulo/rodrigues/librarybookstore/authentication/controller/AuthenticationController.java
@@ -18,31 +18,14 @@ package com.paulo.rodrigues.librarybookstore.authentication.controller;
 
 import com.paulo.rodrigues.librarybookstore.authentication.model.Login;
 import com.paulo.rodrigues.librarybookstore.authentication.model.User;
-import com.paulo.rodrigues.librarybookstore.authentication.repository.UserRepository;
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  *
